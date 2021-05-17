ddba7b4f8fade807e5adc4999c763b3dbe3285af
MunimHalim <g91314@gmail.com>
2021-05-17 23:22:41 +0800

    2

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <ul>
+                <li>
+                    <router-link to="/">Home</router-link>
+                </li>
+                <li>
+                    <router-link to="/about">About</router-link>
+
+                </li>
+            </ul>
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<style>
+    * {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    body {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    #nav {
+        text-align: center;
+        top: 0px;
+        position: fixed;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+        position: sticky;
+        border-right: 1px solid #bbb;
+    }
+
+    li:last-child {
+        border-right: none;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background-color: #111;
+    }
+
+    .active {
+        background-color: #4CAF50;;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>This is the home page</h1>
+        <img @click="turnIt" id="lamb" src="https://www.w3schools.com/js/pic_bulboff.gif">
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+
+    export default {
+        name: 'home',
+        components: {},
+        data() {
+            return {
+                name: "Islam",
+            }
+        },
+        methods: {
+            turnIt() {
+                let lamb = document.getElementById('lamb');
+                if (lamb.src == "https://www.w3schools.com/js/pic_bulboff.gif") {
+                    lamb.src = "https://www.w3schools.com/js/pic_bulbon.gif"
+                } else {
+                    lamb.src = "https://www.w3schools.com/js/pic_bulboff.gif"
+                }
+            }
+        },
+    }
+
+</script>
+<style scoped>
+    img {
+        cursor: pointer;
+    }
+</style>
